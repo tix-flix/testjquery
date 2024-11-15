5cdb107789b7122898e1ee5ee24b236d72bf62e0
tix-flix <timatialiev7@gmail.com>
2024-11-16 03:00:15 +0600

    save me

---
+++ script.home.js
@@ -0,0 +1,18 @@
+$.ajax({
+    type: "GET",
+    url: "./components/dataBase/services.json",
+    success: function (response) {
+        response.map(element => {
+            let characterDiv = `
+            <div class="contJs">
+                <img class="imgjs" width="320" src="${element.image}">
+                <div class="ss">
+                    <h1 class="text">${element.name}</h1>
+                    <h2 class="text">${element.price}</h2>
+                </div>
+            </div>`;
+
+            $(".main__cont").append(characterDiv);
+        });
+    }
+})
---
+++ script.js
@@ -0,0 +1,7 @@
+let btnModal = document.querySelector(".first__btn")
+let modal = document.querySelector(".modal")
+
+
+btnModal.addEventListener("click", () => {
+    modal.classList.toggle("mdlActive")
+})
